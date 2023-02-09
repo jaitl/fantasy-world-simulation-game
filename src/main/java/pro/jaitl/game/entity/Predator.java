3ce0bef8c4c6c0472bf5a8293cb0fd2273047c9b
jaitl <jaitl@outlook.com>
2023-02-09 23:54:09 +0300

    Herbivore move impl

--- src/main/java/pro/jaitl/game/entity/Predator.java
+++ src/main/java/pro/jaitl/game/entity/Predator.java
@@ -1,19 +1,20 @@
 package pro.jaitl.game.entity;
 
 import pro.jaitl.game.map.WorldMap;
-import pro.jaitl.game.params.Params;
 import pro.jaitl.game.params.PredatorParams;
 
 public class Predator extends Creature {
     private final int strength;
+    private final PredatorParams params;
 
     public Predator(PredatorParams params) {
         super("🐺", params.getMaxHealth(), params.getMoveCount());
         this.strength = params.getStrength();
+        this.params = params;
     }
 
     @Override
-    public void makeMove(WorldMap map, Params params) {
-
+    public void makeMove(WorldMap map) {
+            
     }
 }
